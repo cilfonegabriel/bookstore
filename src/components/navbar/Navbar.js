c4ecef7ad9810686fdfff0220385daff4b6773e0
gabicilfone <cilfonegabriel@gmail.com>
2023-01-26 19:28:12 -0300

    change in image, books and navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <div className="nav-container">
@@ -10,15 +11,15 @@ const Navbar = () => (
             <h2>Book Store</h2>
           </li>
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/categories">Categories</a>
+            <Link to="/categories">Categories</Link>
           </li>
         </ul>
       </div>
       <div>
-        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600" alt="logo" />
+        <img src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=600" alt="logo" />
       </div>
     </nav>
   </div>
