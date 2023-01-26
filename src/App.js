7c0362cfd3958d916413098d297730f3d08d5b56
gabicilfone <cilfonegabriel@gmail.com>
2023-01-26 20:16:30 -0300

    fix linter error

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import Categories from './pages/Categories';
+import Home from './pages/home/Home';
+import Categories from './pages/Categories/Categories';
 
 const App = () => (
   <>
--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -6,9 +6,12 @@ const Categories = () => (
     <div>
       <h1>Categories</h1>
       <p>Status</p>
-      <button type="button"> Check Status</button>
+      <button
+        type="button"
+      >
+        Check Status
+      </button>
     </div>
   </section>
 );
-
 export default Categories;
