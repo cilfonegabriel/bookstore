2185beaea07a9630b2e2eb46accb3d46786516c1
gabicilfone <cilfonegabriel@gmail.com>
2023-01-26 16:57:42 -0300

    add Home anda Categories page

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Add from "../addBook/addBook";
+import Books from "../books/books";
+
+const Home = () => (
+    <section>
+        <Books />
+        <Add />
+    </section>
+);
+
+export default Home;
