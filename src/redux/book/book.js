fa00fce02690c7c1d675510d782d5331f259eabd
gabicilfone <cilfonegabriel@gmail.com>
2023-01-30 19:09:19 -0300

    fix linter error

--- src/redux/book/book.js
+++ src/redux/book/book.js
@@ -3,15 +3,15 @@ const REMOVE_A_BOOK = 'REMOVE_A_BOOK';
 const LIST_BOOKS = 'LIST_BOOKS';
 
 const initialState = [{
-    id: '1', title: 'bealy', author: 'father', category: 'java',
+  id: '1', title: 'bealy', author: 'father', category: 'java',
 }];
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_A_BOOK:
-        return [...state, action.book];
+      return [...state, action.book];
     case REMOVE_A_BOOK:
-        return state.filter((book) => book.id !== action.book);
+      return state.filter((book) => book.id !== action.book);
     case LIST_BOOKS:
       return action.book;
     default:
